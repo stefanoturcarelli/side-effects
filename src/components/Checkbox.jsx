95bfb13e9410f986bfaf39ac4d952dde020146c2
Stefano <67341828+stefanoturcarelli@users.noreply.github.com>
2024-06-06 09:13:37 -0500

    Initial commit

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+// The useEffect Hook
+// We use useEffect when a render needs to cause side effects. Think of a side
+// effect as somehting that a function does that it isn't part of the return
+
+// Example: A network request
+
+function Checkbox() {
+  return (
+	<section>
+
+	</section>
+	);
+}
+
+export default Checkbox;
